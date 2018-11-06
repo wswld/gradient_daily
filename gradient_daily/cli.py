9bcf032437b792b93655b913d416e3cc34c6b77a
Vsevolod Glumov <seva17@gmail.com>
2018-11-06 08:09:21 +0300

    #13 implement Instagram auth caching

--- gradient_daily/cli.py
+++ gradient_daily/cli.py
@@ -1,15 +1,82 @@
+import codecs
+import datetime
+import json
+import logging
 import os
 from datetime import date
 from io import BytesIO
 
 import click
-from instagram_private_api import Client, ClientCompatPatch
+from instagram_private_api import (Client, ClientCompatPatch,
+                                   ClientCookieExpiredError, ClientError,
+                                   ClientLoginError, ClientLoginRequiredError)
 
 import gradient_daily
 from gradient_daily import gradient
 
+logger = logging.getLogger(__name__)
+
 TEST_USER_NAME = 'testlogin'
 TEST_PASSWORD = 'testpasswd'
+
+
+def to_json(python_object):
+    if isinstance(python_object, bytes):
+        return {'__class__': 'bytes',
+                '__value__': codecs.encode(python_object, 'base64').decode()}
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+
+def from_json(json_object):
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
+    return json_object
+
+
+def onlogin_callback(api, new_settings_file):
+    cache_settings = api.settings
+    with open(new_settings_file, 'w') as outfile:
+        json.dump(cache_settings, outfile, default=to_json)
+        print('SAVED: {0!s}'.format(new_settings_file))
+
+
+def cached_login_to_instagram(username, password, cache_path):
+    try:
+        if not os.path.isfile(cache_path):
+            # cache file does not exist
+            logger.warning('Unable to find file: {0!s}'.format(cache_path))
+            # login new
+            api = Client(username, password, on_login=lambda x: onlogin_callback(x, cache_path))
+        else:
+            with open(cache_path) as file_data:
+                cached_settings = json.load(file_data, object_hook=from_json)
+            logger.info('Reusing settings: {0!s}'.format(cache_path))
+            device_id = cached_settings.get('device_id')
+            # reuse auth settings
+            api = Client(username, password, settings=cached_settings)
+
+    except (ClientCookieExpiredError, ClientLoginRequiredError) as exc:
+        print('ClientCookieExpiredError/ClientLoginRequiredError: {0!s}'.format(exc))
+        # Login expired
+        # Do relogin but use default ua, keys and such
+        api = Client(username, password, device_id=device_id,
+            on_login=lambda x: onlogin_callback(x, cache_path))
+
+    except ClientLoginError as e:
+        logger.exception('ClientLoginError {0!s}'.format(e))
+        exit(9)
+    except ClientError as e:
+        logger.exception('ClientError {0!s} (Code: {1:d}, Response: {2!s})'.format(e.msg, e.code, e.error_response))
+        exit(9)
+    except Exception as e:
+        logger.exception('Unexpected Exception: {0!s}'.format(e))
+        exit(99)
+
+    # Show when login expires
+    cookie_expiry = api.cookie_jar.auth_expires
+    logger.info('Cookie Expiry: {0!s}'.format(datetime.datetime.fromtimestamp(cookie_expiry).strftime('%Y-%m-%dT%H:%M:%SZ')))
+
+    return api
 
 
 @click.command()
@@ -24,8 +91,7 @@ def generate(login, passwd, debug):
         '#gradient #gradientdaily'.format(date.today(), gradient_daily.__version__) if not debug else ''
     if not login or not passwd:
         raise ValueError('Both login and password are required.')
-    api = Client(username=login, password=passwd)
-    api.login()
+    api = cached_login_to_instagram(username=login, password=passwd, cache_path='instacache.json')
     gr = gradient.Gradient()
     img = gr.img.convert('RGB')
     with BytesIO() as stream:
